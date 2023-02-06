85fe9b5d68d4582bf39b112bb5c3d3f27b613e05
Simone Maestri <simone.maestri@hotmail.it>
2023-02-06 09:42:19 +0100

    Asymmetrical overlap matrix and high-cov genes analyses

--- statistical_analysis.R
+++ statistical_analysis.R
@@ -39,6 +39,16 @@ RRACH_bed <- cbind(as.data.frame(seqnames(RRACH)), start(RRACH), end(RRACH), as.
 colnames(RRACH_bed) <- c("chr", "start", "end", "strand")
 #write.table(RRACH_bed, file = "RRACH_coords.bed", sep = "\t", quote = F, row.names = F)
 
+if (!exists("highcov_bed_file")) {
+  highcov_GRanges <- NULL
+} else {
+  highcov_bed <- read.table(highcov_bed_file, sep = "\t", header = FALSE)
+  highcov_GRanges <- GRanges(seqnames = highcov_bed[, 1],
+                             ranges = IRanges(start = highcov_bed[, 2], end = highcov_bed[, 3]),
+                             strand = highcov_bed[, 6])
+  names(highcov_GRanges) <- highcov_bed[, 4]
+}
+
 ## 2) Binning the genome in windows of length w
 w <- as.numeric(binLength) # binLength parameter from outside
 genesBed <- read.table(genesbed, sep = "\t") # genesbed parameter from outside
@@ -89,34 +99,57 @@ genesBins <- genesBins[which(width(genesBins) == w), ] # Remove bins without len
 
 ###### Find bins with RRACH motifs
 RRACH_granges <- unique(sort(makeGRangesFromDataFrame(RRACH_bed)))
-RRACH_overlap <- findOverlaps(query = RRACH_granges, subject = genesBins, minoverlap = min(5, w), type = "any")
-genesBins_RRACH <- unique(sort(genesBins[subjectHits(RRACH_overlap)]))
-
 # Peaks
 peaks_bed <- read.table(peaks, header = TRUE, sep = "\t") # gold-standard peaks file parameter from outside
 colnames(peaks_bed) <- c("chr", "start", "end", "desc", "score", "strand")
 peaks_granges <- unique(sort(makeGRangesFromDataFrame(peaks_bed)))
+###### Find bins with RRACH motifs
+RRACH_overlap <- findOverlaps(query = RRACH_granges, subject = genesBins, minoverlap = min(5, w), type = "any")
+genesBins_RRACH <- unique(sort(genesBins[subjectHits(RRACH_overlap)]))
+# Peaks
 RRACH_overlap_genesBins_peaks <-  findOverlaps(query = genesBins_RRACH, subject = peaks_granges, minoverlap = 1, type = "any")
 peaks_RRACH_granges <- genesBins_RRACH[sort(unique(queryHits(RRACH_overlap_genesBins_peaks)))]
+
+##### Find bins with high coverage
+
+if (!exists("highcov_bed_file")) {
+  highcov_GRanges <- NULL
+} else {
+  highcov_bed <- read.table(highcov_bed_file, sep = "\t", header = FALSE)
+  highcov_GRanges <- GRanges(seqnames = highcov_bed[, 1],
+                             ranges = IRanges(start = highcov_bed[, 2], end = highcov_bed[, 3]),
+                             strand = highcov_bed[, 6])
+  names(highcov_GRanges) <- highcov_bed[, 4]
+  highcov_overlap <- findOverlaps(query = highcov_GRanges, subject = genesBins, minoverlap = 1, type = "any")
+  genesBins_highcov <- unique(sort(genesBins[subjectHits(highcov_overlap)]))
+  overlap_genesBins_highcov_peaks <-  findOverlaps(query = genesBins_highcov, subject = peaks_granges, minoverlap = 1, type = "any")
+  peaks_highcov_granges <- genesBins_highcov[sort(unique(queryHits(overlap_genesBins_highcov_peaks)))]
+  
+  ###### Find high coverage bins with RRACH motifs
+  highcov_RRACH_overlap <- findOverlaps(query = RRACH_granges, subject = genesBins_highcov, minoverlap = min(5, w), type = "any")
+  genesBins_highcov_RRACH <- unique(sort(genesBins_highcov[subjectHits(highcov_RRACH_overlap)]))
+  genesBins_highcov_RRACH_peaks_overlap <-  findOverlaps(query = genesBins_highcov_RRACH, subject = peaks_granges, minoverlap = 1, type = "any")
+  peaks_highcov_RRACH_granges <- genesBins_highcov_RRACH[sort(unique(queryHits(genesBins_highcov_RRACH_peaks_overlap)))]
+}
 
 Run_statistical_analysis <- function(genesBins_par, peaks_par, files_par, notes = "", w) {
   # Build matrix of zeros
   hitsMatrix <- matrix(c(0), nrow = length(genesBins_par), ncol = length(files_par) + 1)
-  colnames(hitsMatrix) <- c("Gold_standard", basename(files_par))
+  colnames(hitsMatrix) <- c("Reference_set", basename(files_par))
   row.names(hitsMatrix) <- c(1:nrow(hitsMatrix))
   
   peaks_overlap <- findOverlaps(query = peaks_par, subject = genesBins_par, minoverlap = 1, type = "any")
-  hitsMatrix[unique(subjectHits(peaks_overlap)), "Gold_standard"] <- 1
+  hitsMatrix[unique(subjectHits(peaks_overlap)), "Reference_set"] <- 1
   overlapMatrix <- hitsMatrix
   ind_nodef <- setdiff(grep(x = colnames(overlapMatrix), pattern = "nanom6a"), grep(x = colnames(overlapMatrix), pattern = "nanom6a_output\\.bed_ratio\\.0\\.5"))
   if (length(ind_nodef) > 0) {
     overlapMatrix <- overlapMatrix[, -ind_nodef]
   }
   
-  matrix_nanom6A <- hitsMatrix[,"Gold_standard"]
+  matrix_nanom6A <- hitsMatrix[,"Reference_set"]
   names_nanom6A <- c()
   
-  listF1score <- list()
+  Performances <- list()
   listPRcurves <- list()
   
   for (y in files) {
@@ -148,8 +181,6 @@ Run_statistical_analysis <- function(genesBins_par, peaks_par, files_par, notes 
         hitsMatrix[, x] <- rep(-1, length(genesBins_par))
         overlapMatrix[, x] <- rep(0, length(genesBins_par))
       }
-      #noise = 0.00001
-      #hitsMatrix[, x] <- rep((min(score) - noise), length(genesBins_par))
       
       #assign score to hitsMatrix
       hitsMatrix[names(score), x] <- score
@@ -160,8 +191,8 @@ Run_statistical_analysis <- function(genesBins_par, peaks_par, files_par, notes 
       overlapMatrix[pred_pos_def, x] <- 1
       overlapMatrix[pred_neg_def, x] <- 0
       
-      positive <- hitsMatrix[which(hitsMatrix[,"Gold_standard"] == 1), x]
-      negative <- hitsMatrix[which(hitsMatrix[,"Gold_standard"] == 0), x]
+      positive <- hitsMatrix[which(hitsMatrix[,"Reference_set"] == 1), x]
+      negative <- hitsMatrix[which(hitsMatrix[,"Reference_set"] == 0), x]
       #par(mfrow = c(2, 1))
       #pdf(file = paste0(x,"_scores_distribution.pdf"))
       #hist(positive, main = paste0(x, " - Scores for positive peaks"))
@@ -169,6 +200,7 @@ Run_statistical_analysis <- function(genesBins_par, peaks_par, files_par, notes 
       #dev.off()
       if (length(negative) > 0) {
         pr <- pr.curve(scores.class0 = unname(positive), scores.class1 = unname(negative), curve=T, rand.compute=TRUE)
+        save(pr, file = paste0(resultsFolder, "/", x,"_PRcurve", notes, "_window_", w, "bp.Rdata"))
         pdf(file = paste0(resultsFolder, "/", x,"_PRcurve", notes, "_window_", w, "bp.pdf"), width = 8, height = 8)
         plot(pr, main = paste0(x, " Precision-Recall curve"), rand.plot=TRUE)
         dev.off()
@@ -177,9 +209,10 @@ Run_statistical_analysis <- function(genesBins_par, peaks_par, files_par, notes 
         cat(sprintf("All genome bins include peaks, skipping PR curve plotting for file %s\n", x))
       }
       # Plot "manual" PR curve
-      thresholds <- c(seq(from = min(score), to = max(score), length.out = 10000), default_thr)
-      recall <- seq(from = 0, to = 0, length.out = 10001)
-      precision <- seq(from = 0, to = 0, length.out = 10001)
+      thresholds <- c(seq(from = min(score), to = max(score), length.out = 100), default_thr)
+      recall <- seq(from = 0, to = 0, length.out = 101)
+      precision <- seq(from = 0, to = 0, length.out = 101)
+      F1_score <- seq(from = 0, to = 0, length.out = 101)
       for (t in 1:length(thresholds)) {
         thr <- thresholds[t]
         TP <- length(which(positive >= thr))
@@ -188,12 +221,9 @@ Run_statistical_analysis <- function(genesBins_par, peaks_par, files_par, notes 
         TN <- length(which(negative < thr))
         recall[t] <- TP/(TP + FN)
         precision[t] <- TP/(TP + FP)
+        F1_score[t] <- 2*recall[t]*precision[t]/(recall[t] + precision[t])
       }
-      names(recall) <- thresholds
-      names(precision) <- thresholds
-      # F1 score
-      F1score <- 2*(precision[as.character(default_thr)]*recall[as.character(default_thr)])/(precision[as.character(default_thr)]+recall[as.character(default_thr)])
-      listF1score[[x]] <- F1score 
+      Performances[[x]] <- data.frame(tool = x, threshold = thresholds, recall = recall, precision = precision, F1_score = F1_score)      
       #pdf(paste0(x, "_PRcurve_manual.pdf"))
       #plot(recall, precision, main = paste0(x, " - PR manual"), type = "l", xlim = c(0, 1), ylim = c(0, 1))
       #dev.off()
@@ -203,19 +233,18 @@ Run_statistical_analysis <- function(genesBins_par, peaks_par, files_par, notes 
       overlapMatrix[overlap[, 2], x] <- 1
       TP <- 0
       for (y in 1:nrow(hitsMatrix)){
-        if ((hitsMatrix[y, "Gold_standard"] == 1) && (hitsMatrix[y, x] == 1)){
+        if ((hitsMatrix[y, "Reference_set"] == 1) && (hitsMatrix[y, x] == 1)){
           TP <- TP + 1
         }
       }
       # Recall + Precision
-      totPositiveGS <- length(which(hitsMatrix[, "Gold_standard"] == 1))
+      totPositiveGS <- length(which(hitsMatrix[, "Reference_set"] == 1))
       totPositiveTool <- length(which(hitsMatrix[, x] == 1))
       recall <- TP/totPositiveGS
       precision <- TP/totPositiveTool
       # F1 score
-      F1score <- 2*(precision*recall)/(precision+recall)
-      names(F1score) <- "default"
-      listF1score[[x]] <- F1score
+      F1_score <- 2*(precision*recall)/(precision+recall)
+      Performances[[x]]	<- data.frame(tool = x, threshold = thresholds, recall = recall, precision = precision, F1_score = F1_score)
     } 
     else if (grepl(x, pattern = "nanom6a")) {
       hitsMatrix[overlap[, 2], x] <- 1
@@ -225,57 +254,60 @@ Run_statistical_analysis <- function(genesBins_par, peaks_par, files_par, notes 
         overlapMatrix[overlap[, 2], x] <- 1
         TP <- 0
         for (y in 1:nrow(hitsMatrix)){
-          if ((hitsMatrix[y, "Gold_standard"] == 1) && (hitsMatrix[y, x] == 1)){
+          if ((hitsMatrix[y, "Reference_set"] == 1) && (hitsMatrix[y, x] == 1)){
             TP <- TP + 1
           }
         }
         # Recall + Precision
-        totPositiveGS <- length(which(hitsMatrix[, "Gold_standard"] == 1))
+        totPositiveGS <- length(which(hitsMatrix[, "Reference_set"] == 1))
         totPositiveTool <- length(which(hitsMatrix[, x] == 1))
         recall <- TP/totPositiveGS
         precision <- TP/totPositiveTool
         # F1 score
-        F1score <- 2*(precision*recall)/(precision+recall)
-        names(F1score) <- "default"
-        listF1score[[x]] <- F1score
+        F1_score <- 2*(precision*recall)/(precision+recall)
+        Performances[[x]] <- data.frame(tool = x, threshold = "0.5", recall = recall, precision = precision, F1_score = F1_score)
       }
     }
   }
   
-  ### Code for PR curve nanom6A which has multiple files each run with a different threshold
-  names_nanom6A <- str_extract(names_nanom6A, "[0-9]\\.[0-9]*")
-  colnames(matrix_nanom6A) <- c("GS", names_nanom6A)
-  
-  max_thr <- c()
-  short <- matrix_nanom6A[,2:ncol(matrix_nanom6A)]
-  if (length(which(short == 0)) > 0) {
-    max_thr <- vector(length = nrow(short), mode = "numeric")
-    names(max_thr) <- rownames(short)
+  if (length(grep(x = files, pattern = "nanom6A")) > 0) {
+    ### Code for PR curve nanom6A which has multiple files each run with a different threshold
+    names_nanom6A <- str_extract(names_nanom6A, "[0-9]\\.[0-9]*")
+    colnames(matrix_nanom6A) <- c("GS", names_nanom6A)
     
-    short_nozero <- short[names(which(apply(short, 1, function(x) any(as.logical(x)) != 0 ))), ]
-    tmp <- apply(short_nozero, 1, function(x) {names(which(x == 1))[length(which(x == 1))]})
-    max_thr[names(tmp)] <- as.numeric(tmp)
-    max_thr <- unname(max_thr)
-    
-    new_matrix <- cbind(matrix_nanom6A, max_thr)
-    posit <- new_matrix[which(new_matrix[, "GS"] == 1), "max_thr"]
-    negat <- new_matrix[which(new_matrix[, "GS"] == 0), "max_thr"]
-    
-    pr <- pr.curve(posit, negat, curve = T, rand.compute=TRUE)
-    pdf(file = paste0(resultsFolder, "/nanom6A_PRcurve", notes, "_window_", w, "bp.pdf"), width = 8, height = 8)
-    plot(pr, main = "nanom6A Precision-Recall curve", rand.plot=TRUE)
-    dev.off()
-    listPRcurves[["nanom6A_output.bed"]] <- pr
-    #par(mfrow = c(2, 1))
-    #pdf(file = paste0(resultsFolder, "nanom6A_scores_distribution.pdf"))
-    #hist(posit, 100,main = "nanom6A - Scores for positive peaks")
-    #hist(negat, 100,main = "nanom6A - Scores for negative peaks")
-    #dev.off()
-  } else {
-    cat("All genome bins include peaks, skipping PR curve plotting for tool nanom6a\n")
+    max_thr <- c()
+    short <- matrix_nanom6A[,2:ncol(matrix_nanom6A)]
+    if (length(which(short == 0)) > 0) {
+      max_thr <- vector(length = nrow(short), mode = "numeric")
+      names(max_thr) <- rownames(short)
+      
+      short_nozero <- short[names(which(apply(short, 1, function(x) any(as.logical(x)) != 0 ))), ]
+      tmp <- apply(short_nozero, 1, function(x) {names(which(x == 1))[length(which(x == 1))]})
+      max_thr[names(tmp)] <- as.numeric(tmp)
+      max_thr <- unname(max_thr)
+      
+      new_matrix <- cbind(matrix_nanom6A, max_thr)
+      posit <- new_matrix[which(new_matrix[, "GS"] == 1), "max_thr"]
+      negat <- new_matrix[which(new_matrix[, "GS"] == 0), "max_thr"]
+      
+      pr <- pr.curve(posit, negat, curve = T, rand.compute=TRUE)
+      pdf(file = paste0(resultsFolder, "/nanom6A_PRcurve", notes, "_window_", w, "bp.pdf"), width = 8, height = 8)
+      plot(pr, main = "nanom6A Precision-Recall curve", rand.plot=TRUE)
+      dev.off()
+      listPRcurves[["nanom6A_output.bed"]] <- pr
+      #par(mfrow = c(2, 1))
+      #pdf(file = paste0(resultsFolder, "nanom6A_scores_distribution.pdf"))
+      #hist(posit, 100,main = "nanom6A - Scores for positive peaks")
+      #hist(negat, 100,main = "nanom6A - Scores for negative peaks")
+      #dev.off()
+    } else {
+      cat("All genome bins include peaks, skipping PR curve plotting for tool nanom6a\n")
+    }
   }
-  ### Save list of F1 scores
-  capture.output(listF1score, file = paste0(resultsFolder, "/F1score_list_default", notes, "_window_", w, "bp.csv"))
+  
+  sink(paste0(resultsFolder, "/Performances", notes, "_window_", w, "bp.tsv"))
+  print(Performances)
+  sink()
   
   if (length(negative) > 0) {
     ### Plot all the Precision-Recall curves together
@@ -305,27 +337,42 @@ Run_statistical_analysis <- function(genesBins_par, peaks_par, files_par, notes 
   
   for (i in tools) {
     for (j in tools) {
-      data_ovlp[i, j] <- length(intersect(which(overlapMatrix[, i] == 1), which(overlapMatrix[, j] == 1)))/length(union(which(overlapMatrix[, i] == 1), which(overlapMatrix[, j] == 1)))
+      #data_ovlp[i, j] <- length(intersect(which(overlapMatrix[, i] == 1), which(overlapMatrix[, j] == 1)))/length(union(which(overlapMatrix[, i] == 1), which(overlapMatrix[, j] == 1)))
+      data_ovlp[i, j] <- length(intersect(which(overlapMatrix[, i] == 1), which(overlapMatrix[, j] == 1)))/length(which(overlapMatrix[, i] == 1))
     }
   }
-  
   pdf(paste0(resultsFolder, "/Tools_overlap_default_par", notes, "_window_", w, "bp.pdf"))
   pheatmap(data_ovlp, cluster_rows = TRUE, cluster_cols = TRUE, show_rownames = TRUE, show_colnames = TRUE, fontsize = 15, display_numbers = TRUE)
   dev.off()
-
-  return(list(hitsMatrix, overlapMatrix, listF1score, data_ovlp))
+  return(list(hitsMatrix, overlapMatrix, Performances, data_ovlp))
 }
 
 results <- Run_statistical_analysis(genesBins_par = genesBins, peaks_par = peaks_granges, files_par = files, notes = "", w = w)
 hitsMatrix <- results[[1]]
 overlapMatrix <- results[[2]]
-listF1score <- results[[3]]
+Performances <- results[[3]]
 data_ovlp <- results[[4]]
-save(results, file = paste0(resultsFolder, "/Results_window_", w, "bp_results.rda"))
+save(results, file = paste0(resultsFolder, "/Results_window_", w, "bp.rda"))
 
 results_RRACH <- Run_statistical_analysis(genesBins_par = genesBins_RRACH, peaks_par = peaks_RRACH_granges, files_par = files, notes = "_RRACH", w = w)
 hitsMatrix_RRACH <- results_RRACH[[1]]
 overlapMatrix <- results_RRACH[[2]]
-listF1score_RRACH <- results_RRACH[[3]]
+Performances_RRACH <- results_RRACH[[3]]
 data_ovlp_RRACH <- results_RRACH[[4]]
-save(results_RRACH, file = paste0(resultsFolder, "/Results_window_", w, "bp_RRACH_results.rda"))
+save(results_RRACH, file = paste0(resultsFolder, "/Results_window_", w, "bp_RRACH.rda"))
+
+if (!is.null(highcov_GRanges)) {
+  results_highcov <- Run_statistical_analysis(genesBins_par = genesBins_highcov, peaks_par = peaks_highcov_granges, files_par = files, notes = "_highcov", w = w)
+  hitsMatrix_highcov <- results_highcov[[1]]
+  overlapMatrix_highcov <- results_highcov[[2]]
+  Performances_highcov <- results_highcov[[3]]
+  data_ovlp_highcov <- results_highcov[[4]]
+  save(results_highcov, file = paste0(resultsFolder, "/Results_window_", w, "bp_highcov.rda"))
+  
+  results_highcov_RRACH <- Run_statistical_analysis(genesBins_par = genesBins_highcov_RRACH, peaks_par = peaks_highcov_RRACH_granges, files_par = files, notes = "_highcov_RRACH", w = w)
+  hitsMatrix_highcov_RRACH <- results_highcov_RRACH[[1]]
+  overlapMatrix_highcov_RRACH <- results_highcov_RRACH[[2]]
+  Performances_highcov_RRACH <- results_highcov_RRACH[[3]]
+  data_ovlp_highcov_RRACH <- results_highcov_RRACH[[4]]
+  save(results_highcov_RRACH, file = paste0(resultsFolder, "/Results_window_", w, "bp_highcov_RRACH.rda"))
+}
