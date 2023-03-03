9910c5f361a7c3c9188f724eb31c6e5529d8c830
Simone Maestri <simone.maestri@hotmail.it>
2023-03-03 08:21:34 +0100

    Fix default conditions

--- postprocessing.R
+++ postprocessing.R
@@ -514,7 +514,7 @@ output_processing <- function(tool, path_folder, output_file, filtering_paramete
               df_tombo$Pvalue <- 10**(-as.numeric(tombo[names_df_tombo, 5])) # Parameter of filtering is Pvalue not -log10(Pvalue)
               df_tombo_final <- df_tombo[,c(1,2,3,4,8,9)]
               colnames(df_tombo_final) <- c("Chr", "Start", "End", "Strand", "Status", "Pvalue")
-              df_tombo_final$Start <- df_tombo_final$Start
+              df_tombo_final$Start <- df_tombo_final$Start + 1
               df_tombo_final$End <- df_tombo_final$End
               write.table(df_tombo_final, file = output_file, quote = F, sep = "\t", row.names = F)
             }
@@ -616,7 +616,7 @@ pathTools <- c(pathdena, pathdrummer, pathdifferr, pathyanocomp, pathnanocompore
                , pathmines, pathepinanoError, pathepinanoSVM, pathxpore, pathnanodoc, pathnanom6a
                , pathtomboComparison, pathm6anet)
 
-default <- c(0.1, NA, NA, NA, 0.01, 0.001, NA, NA, 0.5, 0.05, 0.02, NA, 0.05, 0.9)
+default <- c(0.1, 0.05, 0.05, 0.05, 0.01, 1, NA, 0.1, 0.5, 0.05, 0.02, NA, 0.05, 0.9)
 relaxed <- c(0, NA, NA, NA, 1, 1, NA, NA, 0, 1, 0, NA, 1, 0)
 value <- rep(threshold, length(default))
 
