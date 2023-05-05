a27641ba07e1f6865c0445905fa1f2b608baed5f
mfurla <mattia.furlan@unito.it>
2023-05-05 09:34:21 +0200

    Update nextflowRun.sh

--- nextflowRun.sh
+++ nextflowRun.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #PBS -S /bin/sh
-#PBS -N bproject
+#PBS -N NanOlympicsMod
 #PBS -l select=1:ncpus=1:mem=2G
 #PBS -M emailAddress
 #PBS -m e
 
 source /path/to/activate /path/to/nextflow/environment
 cd /path/to/pipeline/folder
-nextflow -c pipeline.conf run pipeline.nf -w /path/to/work/folder/ -resume
+nextflow -c pipeline.conf run pipeline.nf -w /path/to/work/folder/
 source /path/to/deactivate
